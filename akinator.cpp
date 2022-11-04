5e60baeb6273a519fd47670f5565a3f9daee666d
diht404 <diht404@gmail.com>
2022-11-04 21:46:12 +0300

    comparison

--- akinator.cpp
+++ akinator.cpp
@@ -62,11 +62,10 @@ size_t selectTask(Tree *tree)
 
                 char second_entity[BUFFER_SIZE] = "";
                 printf("Enter first entity: \n");
-                skipUnusedSymbols();
                 size_t second_len = gets(second_entity);
                 second_entity[second_len - 1] = '\0';
 
-//                getComparison(tree, first_entity, second_entity);
+                getComparison(tree, first_entity, second_entity);
                 break;
             }
             case 4:
@@ -157,72 +156,179 @@ size_t treeGuessing(Tree *tree)
     return TREE_NO_ERRORS;
 }
 
+size_t getComparison(Tree *tree,
+                     Val_t first_entity,
+                     Val_t second_entity)
+{
+    if (strcasecmp(first_entity, second_entity) == 0)
+    {
+        printf("It's the same. What you want yo compare?..\n");
+        return TREE_NO_ERRORS;
+    }
+
+    Stack stack = {};
+    size_t error = STACK_NO_ERRORS;
+    stackCtor(&stack, 0, &error)
+
+    Node *definition_node = pushUntilValue(tree, &stack, first_entity);
+    if (definition_node == nullptr)
+    {
+        printf("I don't know what is %s.\n", first_entity);
+        return TREE_NO_ERRORS;
+    }
+    if (stack.size == 0)
+    {
+        printf("%s is %s\n", first_entity, first_entity);
+        return TREE_NO_ERRORS;
+    }
+
+    Definition definition_first = {};
+    createDefinition(&stack, definition_node, &definition_first);
+
+    definition_node = pushUntilValue(tree, &stack, second_entity);
+    if (definition_node == nullptr)
+    {
+        printf("I don't know what is %s.\n", second_entity);
+        return TREE_NO_ERRORS;
+    }
+    if (stack.size == 0)
+    {
+        printf("%s is %s\n", second_entity, second_entity);
+        return TREE_NO_ERRORS;
+    }
+
+    Definition definition_second = {};
+    createDefinition(&stack, definition_node, &definition_second);
+
+    printf("Common between %s and %s:",
+           first_entity,
+           second_entity);
+
+    int counter_first = definition_first.length - 1;
+    int counter_second = definition_second.length - 1;
+
+    while ((strcasecmp(
+        definition_first.definition[counter_first],
+        definition_second.definition[counter_second]) == 0)
+        and counter_first >= 0
+        and counter_second >= 0)
+    {
+        printf("%s%s",
+               (counter_first == definition_first.length - 1) ? " "
+                                                              : " and",
+               definition_first.definition[counter_first]);
+
+        counter_first--;
+        counter_second--;
+    }
+    if (counter_first != definition_first.length - 1)
+        printf(".\n");
+    else
+        printf(" nothing common...\n");
+
+    printf("Difference between %s and %s:\n",
+           first_entity,
+           second_entity);
+
+    if (counter_first>=0)
+        printf("%s:", first_entity);
+    for (int i = counter_first; i >= 0; i--)
+    {
+        printf("%s%s", definition_first.definition[i], i ? " and" : ".\n");
+    }
+
+    if (counter_second>=0)
+        printf("%s:", second_entity);
+    for (int i = counter_second; i >= 0; i--)
+    {
+        printf("%s%s", definition_second.definition[i], i ? " and" : ".\n");
+    }
+}
+
 size_t getDefinition(Tree *tree, Val_t value)
 {
     Stack stack = {};
     size_t error = STACK_NO_ERRORS;
     stackCtor(&stack, 0, &error)
 
-    pushPointersToStack(tree->root, &stack, value);
+    Node *definition_node = pushUntilValue(tree, &stack, value);
+
+    if (definition_node == nullptr)
+    {
+        printf("I don't know what is it.\n");
+        return TREE_NO_ERRORS;
+    }
     if (stack.size == 0)
-        return TREE_NO_SUCH_ELEMENT;
+    {
+        printf("%s is %s\n", value, value);
+        return TREE_NO_ERRORS;
+    }
+    printf("%s is", value);
+
+    Definition definition = {};
+
+    createDefinition(&stack, definition_node, &definition);
+
+    for (int i = definition.length - 1; i >= 0; i--)
+    {
+        printf("%s%s", definition.definition[i], i ? " and" : ".\n");
+    }
+
+    return error;
+}
+
+Node *pushUntilValue(Tree *tree, Stack *stack, Val_t value)
+{
+    pushPointersToStack(tree->root, stack, value);
+    if (stack->size == 0)
+        return nullptr;
 
     Node *definition_node = nullptr;
     Elem_t stackValue = 0;
-    while (stack.size)
+    while (stack->size)
     {
-        stackPop(&stack, &stackValue);
+        stackPop(stack, &stackValue);
         if (strcasecmp(((Node *) stackValue)->value, value) == 0)
         {
             definition_node = (Node *) stackValue;
-            printf("%s is", value);
+//            printf("%s is", value);
             break;
         }
     }
-    if (definition_node == nullptr)
-    {
-        printf("I don't know what is it.");
-        return TREE_NO_ERRORS;
-    }
-    if (stack.size == 0)
-    {
-        printf(" %s\n", value);
-        return TREE_NO_ERRORS;
-    }
+    return definition_node;
+}
 
-
-
+size_t createDefinition(Stack *stack,
+                        Node *definition_node,
+                        Definition *definition)
+{
+    Elem_t stackValue = 0;
     Node *candidate_definition_node = nullptr;
-    char definition[BUFFER_SIZE][BUFFER_SIZE] = {};
-    int counter = 0;
-    while (stack.size)
+
+    while (stack->size)
     {
-        stackPop(&stack, &stackValue);
+        stackPop(stack, &stackValue);
         candidate_definition_node = (Node *) stackValue;
         if (candidate_definition_node->left == definition_node)
         {
             definition_node = candidate_definition_node;
-            sprintf(definition[counter], " %s",
+            sprintf(definition->definition[definition->length],
+                    " %s",
                     definition_node->value);
-            counter++;
+            definition->length++;
             continue;
         }
         else if (candidate_definition_node->right == definition_node)
         {
             definition_node = candidate_definition_node;
-            sprintf(definition[counter],
+            sprintf(definition->definition[definition->length],
                     " not %s",
                     definition_node->value);
-            counter++;
+            definition->length++;
             continue;
         }
     }
-    for (int i = counter - 1; i>=0; i--)
-    {
-        printf("%s%s", definition[i], i ? " and": ".\n");
-    }
 
-    return error;
 }
 
 size_t pushPointersToStack(Node *node, Stack *stack, Val_t value)
--- akinator.h
+++ akinator.h
@@ -12,11 +12,25 @@
 const size_t BUFFER_SIZE = 128;
 const char AKINATOR_FILE[] = "tree.txt";
 
+struct Definition
+{
+    char definition[BUFFER_SIZE][BUFFER_SIZE] = {};
+    int length = 0;
+};
+
 size_t selectTask(Tree *tree);
 
 size_t treeGuessing(Tree *tree);
 
+size_t getComparison(Tree *tree, Val_t first_entity, Val_t second_entity);
+
 size_t getDefinition(Tree *tree, Val_t value);
+
+Node *pushUntilValue(Tree *tree, Stack *stack, Val_t value);
+
+size_t createDefinition(Stack *stack,
+                        Node *definition_node,
+                        Definition *definition);
 
 size_t pushPointersToStack(Node *node, Stack *stack, Val_t value);
 
